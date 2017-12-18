42c3243b7e8072bd94825f09126fe7a2127742d2
ika.grabon <ika.grabon@gmail.com>
2017-12-18 17:00:23 +0100

    Add bottles method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,11 +1,24 @@
 class Bottles
+
   def song
     puts 'song'
   end
+
   def verse(v)
     puts 'verse'
   end
+
   def verses(v, w)
     puts 'more verses'
+  end
+
+  def bottles(num_of_bottles)
+    if num_of_bottles > 1
+      "#{num_of_bottles} bottles"
+    elsif num_of_bottles == 1
+      "#{num_of_bottles} bottle"
+    else
+      "no more bottles"
+    end
   end
 end
